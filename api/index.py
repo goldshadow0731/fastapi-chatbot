2043489981933c667f1841e2d4ef63bda64a8ae4
goldshadow <davidfang148@gmail.com>
2022-12-24 19:03:55 +0800

    test

--- api/index.py
+++ api/index.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
-from fastapi import FastAPI
-# import uvicorn
+import os
+
+from fastapi import FastAPI, Request, HTTPException
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import openai
 
 
 # FastAPI
 app = FastAPI()
+
+# OpenAI
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Line Bot
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 
 
 @app.get("/")
@@ -12,5 +24,27 @@ def index():
     return "OK"
 
 
-# if __name__ == "__main__":
-#     uvicorn.run(app=app, port=80, log_level="info")
+@app.post("/webhook")
+async def webhook(request: Request):
+    signature = request.headers["X-Line-Signature"]
+    body = await request.body()
+    try:
+        handler.handle(body.decode(), signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="Missing Parameters")
+    return "OK"
+
+
+@handler.add(MessageEvent, message=(TextMessage))
+def handling_message(event):
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=event.message.text,
+        max_tokens=256,
+        temperature=0.5,
+    )
+    line_bot_api.reply_message(
+        reply_token=event.reply_token,
+        messages=TextSendMessage(
+            text=response["choices"][0]["text"].replace('\n', ''))
+    )
